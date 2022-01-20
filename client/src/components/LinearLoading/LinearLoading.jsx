59d4817ec3bed7537d551df4b05125aa9caad3dd
Puneet Goel <puneetgoel016@gmail.com>
2022-01-20 13:09:34 +0530

    improved linear loading

--- client/src/components/LinearLoading/LinearLoading.jsx
+++ client/src/components/LinearLoading/LinearLoading.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { LinearProgress } from '@material-ui/core';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import { LinearProgress, Typography } from '@material-ui/core';
 import memoriesText from "./memoriesText.png";
 import { authenticate } from "../../actions/auth";
 import { useDispatch } from "react-redux";
@@ -17,15 +17,30 @@ const useStyles = makeStyles(() => ({
 		alignItems: 'center',
 		paddingLeft: '10px',
 		paddingRight: '10px',
-	}, 
-	line: {
-		width: '100%',
-	}
+	},
 }));
+
+const BorderLinearProgress = withStyles((theme) => ({
+	root: {
+		height: 10,
+		width: '100%',
+		borderRadius: 5,
+	},
+	colorPrimary: {
+		backgroundColor: theme.palette.grey[theme.palette.type === 'light' ? 200 : 700],
+	},
+	bar: {
+		borderRadius: 5,
+		backgroundColor: '#1a90ff',
+	},
+}))(LinearProgress);
 
 const LinearLoading = ({ setUserValid }) => {
 	const classes = useStyles();
 	const [progress, setProgress] = useState(0);
+	const [jump, setJump] = useState(2);
+	const [auth, setAuth] = useState('auth');
+
 	const dispatch = useDispatch();
 	const navigate = useNavigate();
 
@@ -33,27 +48,47 @@ const LinearLoading = ({ setUserValid }) => {
 
 		(async () => {
             const x = await dispatch(authenticate());
-
-			if(!x){
-				navigate('/login');
-			}
-            setUserValid(x);
+			setJump(30);
+			setAuth(x);
         })()
 
+	}, [navigate, dispatch]);
+
+	useEffect(() => {
+		if(progress >= 100 && auth === false){
+			navigate('/login');
+		}else if(progress >= 100 && auth === true){
+			setUserValid(true);
+		}else if(progress >= 100){
+			alert('There is some problem while loading resources for you. Check your Internet connection, and try again later.');
+			setProgress(0);
+			setJump(2);
+		}
+	}, [progress, auth, navigate, setUserValid]);
+
+	useEffect(() => {
 		const timer = setInterval(() => {
-			setProgress((prevProgress) => (prevProgress >= 100 ? 0 : prevProgress + 20));
+			
+			setProgress((prevProgress) => {
+				return (prevProgress >= 100 ? 100 : (prevProgress + jump)>=100? 100 : (prevProgress + jump));
+			});
+			
 		}, 300);
 
 		return () => {
 			clearInterval(timer);
 		};
-	}, [navigate, setUserValid, dispatch]);
+	}, [navigate, dispatch, jump]);
 
 	return (
 		<div className={classes.mainLine}>
 			<img src={memoriesText} alt="icon" width="100%" />
 			<br />
-			<LinearProgress className={classes.line} variant="determinate" value={progress} />
+			<BorderLinearProgress variant="determinate" value={progress} />
+			<br />
+			<Typography variant="body2" color="textSecondary">
+				{`${Math.round(progress)}%`}
+			</Typography>
 		</div>
 	);
 }
